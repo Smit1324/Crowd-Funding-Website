dcf489e8185e1d90766b8910b2f3a40fd4acf77a
Smit <smitmpatil123@gmail.com>
2023-01-24 10:16:53 +0530

    Commit

--- src/components/compo/Navbar.js
+++ src/components/compo/Navbar.js
@@ -15,9 +15,6 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <NavLink className="navbar-brand" to="/"><img src={logo} className='logo' alt='Shayar' /></NavLink >
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item mx-4">
